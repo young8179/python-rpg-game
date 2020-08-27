4cb65c4ea6e39b6ad3ceaad4c9aa62fe33ac4db1
Heeyoung Song <hsong8179@gmail.com>
2020-08-26 21:03:50 -0400

    added rpg exercise

---
+++ classes_rpg.py
@@ -0,0 +1,50 @@
+
+class Character(Hero, Goblin):
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+  
+
+
+class Hero:
+    
+    
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+        
+    
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print(f"You do {self.power} damage to the goblin." )
+    
+  
+            
+    
+    def print_status(self):
+        print(f"""{self.name}'s health: {self.health}, \n{self.name}'s power: {self.power}\n""")
+
+
+class Goblin:
+    
+    
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+        
+        
+    
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print(f"goblin do {self.power} damage to the hero." )
+    
+    
+    def print_status(self):
+        print(f"""\n{self.name}'s health: {self.health},\n{self.name}'s power: {self.power}""")
+
+
+
---
+++ copy.py
@@ -0,0 +1,56 @@
+
+class Character:
+    def is_alive(self):
+        if self.health > 0:
+            return True
+        else:
+            return False
+    def attack(self, enemy):
+        if self == Hero:
+            enemy.health -= self.power
+            print(f"You do {self.power} damage to the goblin." )       
+        elif self == Goblin:
+            enemy.health -= self.power
+            print(f"You do {self.power} damage to the goblin." )  
+
+
+class Hero(Character):
+    
+    
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+        
+    
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print(f"You do {self.power} damage to the goblin." )
+    
+  
+            
+    
+    def print_status(self):
+        print(f"""{self.name}'s health: {self.health}, \n{self.name}'s power: {self.power}\n""")
+
+
+class Goblin(Character):
+    
+    
+    def __init__(self, name, health, power):
+        self.name = name
+        self.health = health
+        self.power = power
+        
+        
+    
+    def attack(self, enemy):
+        enemy.health -= self.power
+        print(f"You do {self.power} damage to the hero." )
+    
+    
+    def print_status(self):
+        print(f"""\n{self.name}'s health: {self.health},\n{self.name}'s power: {self.power}""")
+
+
+
